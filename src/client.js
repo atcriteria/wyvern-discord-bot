09a307d138c0a2da7a1121a67b8efc23579a787c
dude lao <brycelipscomb@gmail.com>
2022-03-25 22:11:59 -0400

    added support to forward messages to Toe's server when the game sends a DiscordWebhook so they can see it too

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 // The main client for the bot.
 
 const { Client, Intents} = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS], partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILDS], partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
 
 module.exports = client;
---
+++ src/events/onMessage.js
@@ -0,0 +1,41 @@
+const { MessageEmbed, WebhookClient } = require('discord.js')
+const webhookId = process.env.TOE_WEBHOOK_ID
+const webhookToken = process.env.TOE_WEBHOOK_TOKEN
+
+
+// Currently hardwired to forward the Wyvern Game's DiscordWebhook message to
+// Toe's Discord channel. For future use with the 'messageCreate' event you
+// should pull the code that handles this functionality to its own module
+// and import it back to clean this up.
+module.exports = {
+	name: 'messageCreate',
+	async execute(message){
+        try {
+            const channel = message.channelId;
+            const lqannouncements = "900086774141956216"
+            // The incoming webhook id
+            const hookId = message.webhookId;
+            // If the message was created in the LQ-Announcements channel and it is a Webhook message
+            if (channel === lqannouncements && hookId){
+                console.log("Hooked message, forward to other server")
+                const webhook = new WebhookClient({id: webhookId, token: webhookToken});
+                // This has the server time in it
+                const originFields = message.embeds.fields
+                const newEmbeds = new MessageEmbed()
+                    .setTitle("Automated Live Quest Starting")
+                    .setDescription("Work together with other players to overcome unique challenges or defeat powerful foes.")
+                    .setColor("#0099ff")
+                    .setImage("https://i.pinimg.com/550x/65/e3/4b/65e34b409feb6d86376b9de87e4c08c3.jpg")
+                    .setFields(originFields)
+                const newMessage = {
+                    username: "Wyvern Automated Live Quest Messaging System",
+                    avatarURL: "https://i.imgur.com/lna0M5y.png",
+                    embeds: [newEmbeds]
+                }
+                webhook.send(newMessage).catch(console.error);
+            }
+        } catch (e) {
+            console.error(e)
+        }
+	},
+};
