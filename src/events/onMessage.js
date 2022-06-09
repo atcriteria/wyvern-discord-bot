4675cffc62c2561360bf99871bed1de51ba68f86
dude lao <brycelipscomb@gmail.com>
2022-06-09 17:07:53 -0400

    handling for incoming LDR messages. I want to make sure the bot intercepts them when received from the Wyvern DiscordWebhook and clears the content before forwarding what the actual content will be to the proper endpoint.

--- src/events/onMessage.js
+++ src/events/onMessage.js
@@ -10,6 +10,7 @@ const webhookToken = process.env.TOE_WEBHOOK_TOKEN
 module.exports = {
 	name: 'messageCreate',
 	async execute(message){
+        console.log(message);
         try {
             const channel = message.channelId;
             const lqannouncements = "900086774141956216"
@@ -17,24 +18,29 @@ module.exports = {
             const hookId = message.webhookId;
             // If the message was created in the LQ-Announcements channel and it is a Webhook message
             if (channel === lqannouncements && hookId){
-                console.log("Hooked message, forward to other server")
-                const webhook = new WebhookClient({id: webhookId, token: webhookToken});
-                const originalEmbed = message.embeds[0];
-                const originalFields = originalEmbed.fields;
-                console.log(originalFields)
-                console.log(originalEmbed)
-                const newEmbeds = new MessageEmbed()
-                    .setTitle(originalEmbed.title)
-                    .setDescription(originalEmbed.description)
-                    .setColor("#0099ff")
-                    .setImage("https://i.pinimg.com/550x/65/e3/4b/65e34b409feb6d86376b9de87e4c08c3.jpg")
-                    .setFields(originalFields)
-                const newMessage = {
-                    username: "Wyvern Automated Live Quest Messaging System",
-                    avatarURL: "https://i.imgur.com/lna0M5y.png",
-                    embeds: [newEmbeds]
+                if (message.content === "LRD"){
+                    console.log("Sending message to Application, shouldn't see discord message");
+                    message.content = "";
+                } else {
+                    console.log("Hooked message, forward to other server")
+                    const webhook = new WebhookClient({id: webhookId, token: webhookToken});
+                    const originalEmbed = message.embeds[0];
+                    const originalFields = originalEmbed.fields;
+                    console.log(originalFields)
+                    console.log(originalEmbed)
+                    const newEmbeds = new MessageEmbed()
+                        .setTitle(originalEmbed.title)
+                        .setDescription(originalEmbed.description)
+                        .setColor("#0099ff")
+                        .setImage("https://i.pinimg.com/550x/65/e3/4b/65e34b409feb6d86376b9de87e4c08c3.jpg")
+                        .setFields(originalFields)
+                    const newMessage = {
+                        username: "Wyvern Automated Live Quest Messaging System",
+                        avatarURL: "https://i.imgur.com/lna0M5y.png",
+                        embeds: [newEmbeds]
+                    }
+                    webhook.send(newMessage).catch(console.error);
                 }
-                webhook.send(newMessage).catch(console.error);
             }
         } catch (e) {
             console.error(e)
