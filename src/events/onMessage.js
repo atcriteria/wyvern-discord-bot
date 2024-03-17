9f74c535e3c1c3d9bdf78f2c22ff948ac1c6497e
Bryce Lipscomb <brycelipscomb@gmail.com>
2024-03-17 13:48:46 -0400

    Discord bot now posts to Reddit from the news channel

--- src/events/onMessage.js
+++ src/events/onMessage.js
@@ -1,6 +1,15 @@
 const { MessageEmbed } = require('discord.js')
 const { getLQData } = require("../util/data")
+// Reddit wrapper
+const snoowrap = require('snoowrap');
 
+const reddit = new snoowrap({
+    userAgent: process.env.REDDIT_USER_AGENT,
+    clientId: process.env.REDDIT_CLIENT_ID,
+    clientSecret: process.env.REDDIT_CLIENT_SECRET,
+    username: process.env.REDDIT_USERNAME,
+    password: process.env.REDDIT_PASSWORD,
+});
 
 // No longer supporting the forwarding to other servers
 // In the future, the Wyvern Server may become a community server
@@ -11,8 +20,35 @@ module.exports = {
         try {
             const channel = message.channelId;
             const lqannouncements = "900086774141956216"
+            const newsChannel = "718986723253682317"
+            const newsRole = "1218947003552104598"
             // The incoming webhook id
             const hookId = message.webhookId;
+
+            // Handle the flow of pasting news messages to Reddit
+            if (channel === newsChannel){
+                if (message.mentions.roles && !message.mentions.roles.has(newsRole)) return;
+                let author = message.author.username;
+                // Only let me and Rizato post to Reddit from Discord.
+                if (author.toLowerCase() != "archanine" && author.toLowerCase() != "rizato") return;
+                if (message.guild){
+                    author = message.member.nickname;
+                }
+                let date = new Date();
+                let month = String(date.getMonth() + 1).padStart(2, '0')
+                let day = String(date.getDate()).padStart(2, '0')
+                let year = date.getFullYear()
+                const content = message.content;
+                // Regular expression to match Discord role mentions
+                const roleMentionRegex = /<@&\d+>\s*/g;
+                // Replace the role mention with an empty string
+                const trimmedContent = content.replace(roleMentionRegex, '');
+
+                reddit.getSubreddit("wyvernrpg").submitSelfpost({
+                    title: `Wyvern Patch Notes (${author}): ${month}/${day}/${year}`,
+                    text: trimmedContent
+                }).then(submission => console.log('Posted to Reddit', submission.url)).catch(console.error)
+            }
             // If the message was created in the LQ-Announcements channel and it is a Webhook message
             if (channel === lqannouncements && hookId){
                 console.log(message);
