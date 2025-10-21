ff53b323dddb5b980d20b187641e73e724c91902
Bryce Lipscomb <brycelipscomb@gmail.com>
2025-10-21 15:40:10 -0400

    registering test command

---
+++ src/commands/test.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('test')
+        .setDescription('This is a test'),
+    async execute(interaction) {
+        try {
+            await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`)
+        } catch (e) {
+            console.error(e)
+        }
+    },
+};
